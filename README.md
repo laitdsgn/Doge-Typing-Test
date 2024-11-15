a3b2dd6ba882ec2db89eb9fc48c5eda4d1c968bd
laitdsgn <tomek08.konto@gmail.com>
2024-11-15 09:24:43 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Typing-Speed-Test
+# Writing Speed Tester
  
+This is an app where you can test your typing speed based on how many words you can type in one minute.
---
+++ script.js
@@ -0,0 +1,16 @@
+
+const texts = [
+"Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptates quibusdam doloribus excepturi eius culpa eum ex exercitationem architecto repellendus veniam eaque, eos voluptate. Enim modi libero non debitis incidunt?",
+"Speed typing combines precision, rhythm, and muscle memory. It's a critical skill for programmers, enabling them to focus on logic rather than keystrokes. But speed isn't everything—accuracy is just as important. A single typo can cause bugs, making deliberate practice essential for typing success.",
+"Fast typing boosts efficiency in programming, helping developers quickly write and debug code. Switching between syntax elements and using shortcuts demands both speed and accuracy. During intense coding sessions, balancing speed with careful problem-solving is key to great results.",
+"She sells seashells by the seashore, but the seashells she sells aren't just any seashells—they're specially selected seashells that shimmer and shine by the sunny seashore. If she selects shimmering seashells to sell by the seashore, should she sell seashells that shimmer more or seashells that shine less?"
+
+]
+
+function randomText() {
+
+    let rand = Math.floor(Math.random() * texts.length)
+
+    
+    
+}
